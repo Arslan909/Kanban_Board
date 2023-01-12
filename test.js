da2f4d214abdd886cd6c3759f77dee933e31ce65
Arslan909 <arslanswl909@gmail.com>
2023-01-12 16:46:50 +0500

    created function to add task

--- test.js
+++ test.js
@@ -1,64 +1,20 @@
-let obj = {
+const obj = {
 
-    // todo-tasks array
-    array1: [
-        {
-            description: "task to-do 1",
-            date_created: "11-05-2023",
-            isMarked: false
+  // todo-tasks array
+  todo_board: [],
+  // in-progress tasks array
+  inProgress_board: [],
+  // completed Tasks array
+  Done_board: [],
+};
 
-        },
-        {
-            description: "task to-do 2",
-            date_created: "11-05-2023",
-            isMarked: false
+function addTask(description1, date, isMarked1, board) {
+  const task = {
+    description: description1,
+    date_created: date,
+    isMarked: isMarked1,
+  };// creting new object
 
-        }
-
-
-    ],
-    // in-progress tasks array
-    array2: [
-        {
-            description: "task in-progress 1",
-            date_created: "11-05-2023",
-            isMarked: false
-
-        },
-        {
-            description: "task in-progress 2",
-            date_created: "11-05-2023",
-            isMarked: false
-
-        }
-    ],
-    // completed Tasks array
-    array3: [
-        {
-            description: "task completed 1",
-            date_created: "11-05-2023",
-            isMarked: false
-
-        },
-        {
-            description: "task completed 2",
-            date_created: "11-05-2023",
-            isMarked: false
-
-        }
-    ],
-
-    // Funtions of the object contaning 3 array
-
-    taskAdd: function (description1, date_created1, isMarked1) {
-        New_obj = { description: description1, date_created: date_created1, isMarked: isMarked1 }// creting new object
-        this.array1.push(New_obj);//adding new object to the To-do tasks array
-
-    },
-    // taskRemove : function(){},
-    // taskMove : function(){}
+  board.push(task);
 }
-
-// console.log(obj.taskAdd("test","11-jan-2023",false))
-
-//console.log(obj.array1)
+addTask('test', '12-jan-2023', false, obj.todo_board);
