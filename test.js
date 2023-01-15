ad8bc2a81a19df24bcbd36389b0f5d157eca336d
MTS999 <mtsprogrammer999@gmail.com>
2023-01-16 01:35:13 +0500

    change function name to modifyTask refectored moveTask function

--- test.js
+++ test.js
@@ -8,29 +8,30 @@ const Board = {
   // completed Tasks array
   done: [],
 };
-
+// function to add task
 const addTask = (description1, date, mark, board) => {
   const task = {
     description: description1,
     date_created: date,
     isMarked: mark,
-  };// creting new Boardect
-
+  };
   board.push(task);
 };
 
+// function to remove task
 const removeTask = (board, index) => {
   board.splice(index, 1);
 };
 
-removeTask(Board.todo, 1);
-
-const move = (sourceBoard, destinationBoard) => {
-  destinationBoard.push(sourceBoard);
+// function to move task
+const moveTask = (sourceBoard, destinationBoard, index) => {
+  destinationBoard.push(sourceBoard[index]);
+  removeTask(sourceBoard, index);
 };
 
-const modification = (description1, date, mark, index) => {
+// function to modify task
+const modifyTask = (description1, date, mark, index) => {
   Board.todo[index].description = description1;
-  Board.todo[index].date_created = date;/* eslint-disable no-unused-vars */
+  Board.todo[index].date_created = date;
   Board.todo[index].isMarked = mark;
 };
